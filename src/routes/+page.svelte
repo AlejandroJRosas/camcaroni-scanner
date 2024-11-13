6d6ccff30e1db27d4d3970b23963ebc1bdb22bcb
Alejandro Rosas <alejandrojrosas2001@gmail.com>
2024-11-13 07:01:03 -0400

    fix: camera permissions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,7 @@
 	function startScanning() {
 		if (isScanning) return;
 
+		lastScannedText = '';
 		isScanning = true;
 		const config = { fps: 10, qrbox: 250 };
 		scanner = new Html5Qrcode('reader');
@@ -36,7 +37,29 @@
 	}
 
   onMount(() => {
-    startScanning();
+    isScanning = true;
+		const config = { fps: 10, qrbox: 250 };
+		scanner = new Html5Qrcode('reader');
+
+		scanner
+			.start(
+				{ facingMode: 'environment' },
+				config,
+				(decodedText, _decodedResult) => {
+					lastScannedText = `${decodedText}&key=${PERMISSION_KEY}`;
+					isScanning = false;
+					if (scanner) {
+						scanner.stop();
+					}
+				},
+
+				(errorMessage) => {
+					console.warn(`QR Code scan error: ${errorMessage}`);
+				}
+			)
+			.catch((err) => {
+				console.error(`Unable to start scanning: ${err}`);
+			});
   });
 
 	onDestroy(() => {
@@ -54,14 +77,14 @@
 </script>
 
 {#if lastScannedText}
-	<p>Scanned Text: {lastScannedText}</p>
+	<p class="result">Participante Nro: {lastScannedText.slice(lastScannedText.indexOf("id=")+3, lastScannedText.indexOf("id=")+7)}</p>
+{/if}
+<div id="reader" class="scanner"></div>
+{#if lastScannedText}
 	<a href={lastScannedText} target="_blank" class="anchor" referrerpolicy="no-referrer"
 		>Marcar asistencia</a
 	>
 	<button on:click={startScanning}>Scan New QR Code</button>
-{/if}
-{#if isScanning}
-	<div id="reader" class="scanner"></div>
 {/if}
 
 <style>
@@ -79,6 +102,11 @@
 		width: 300px;
 		border: 1px solid white;
 		margin: 1rem 0;
+	}
+
+	.result {
+		margin: 1rem 0;
+		text-align: center;
 	}
 
 	button {
