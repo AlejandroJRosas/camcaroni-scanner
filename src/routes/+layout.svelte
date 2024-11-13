4b3f48e62ded3ef64b28f874ec3bbc61422f3a2c
Alejandro Rosas <alejandrojrosas2001@gmail.com>
2024-11-13 06:24:44 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<style>
+
+	:global(html, body) {
+		@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+		font-family: "Poppins", sans-serif;
+		background-color: #152a55;
+		color: white;
+		margin: 0;
+		height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+
+		.brand {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			margin-bottom: 0.5rem;
+
+			.circle {
+				width: 4rem;
+				height: 4rem;
+				border-radius: 100%;
+				border-top: transparent 0.75rem solid;
+				border-bottom: white 0.75rem solid;
+				border-left: white 0.75rem solid;
+				border-right: white 0.75rem solid;
+				background-color: transparent;
+				margin-right: 0.5rem;
+				position: relative;
+			}
+
+			.circle::before {
+				content: "";
+				width: 1.5rem;
+				height: 1.5rem;
+				border-radius: 100%;
+				background-color: white;
+				position: absolute;
+				top: 50%;
+				left: 50%;
+				transform: translate(-50%, -50%);
+			}
+
+			.title {
+				width: 13rem;
+				font-size: 1.5rem;
+				line-height: 2rem;
+			}
+		}
+	}
+
+</style>
+
+<div class="brand">
+	<div class="circle"></div>
+	<h1 class="title">Expo Empresarial Camcaroní 2024</h1>
+</div>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+	import { Html5Qrcode } from 'html5-qrcode';
+
+	const PERMISSION_KEY = import.meta.env.VITE_PERMISSION_KEY;
+	let scanner: Html5Qrcode | null = null;
+  let lastScannedText = '';
+	let isScanning = false;
+
+	function startScanning() {
+		if (isScanning) return;
+
+		isScanning = true;
+		const config = { fps: 10, qrbox: 250 };
+		scanner = new Html5Qrcode('reader');
+
+		scanner
+			.start(
+				{ facingMode: 'environment' },
+				config,
+				(decodedText, _decodedResult) => {
+					lastScannedText = `${decodedText}&key=${PERMISSION_KEY}`;
+					isScanning = false;
+					if (scanner) {
+						scanner.stop();
+					}
+				},
+
+				(errorMessage) => {
+					console.warn(`QR Code scan error: ${errorMessage}`);
+				}
+			)
+			.catch((err) => {
+				console.error(`Unable to start scanning: ${err}`);
+			});
+	}
+
+  onMount(() => {
+    startScanning();
+  });
+
+	onDestroy(() => {
+		if (scanner) {
+			scanner
+				.stop()
+				.then(() => {
+					console.log('QR Code scanning stopped.');
+				})
+				.catch((err) => {
+					console.error(`Error stopping the scanner: ${err}`);
+				});
+		}
+	});
+</script>
+
+{#if lastScannedText}
+	<p>Scanned Text: {lastScannedText}</p>
+	<a href={lastScannedText} target="_blank" class="anchor" referrerpolicy="no-referrer"
+		>Marcar asistencia</a
+	>
+	<button on:click={startScanning}>Scan New QR Code</button>
+{/if}
+{#if isScanning}
+	<div id="reader" class="scanner"></div>
+{/if}
+
+<style>
+	.anchor {
+		color: white;
+		text-decoration: none;
+		font-size: 1.5rem;
+		margin: 0.5rem 0;
+		border: 1px solid white;
+		padding: 0.5rem 1rem;
+		border-radius: 0.5rem;
+	}
+
+	.scanner {
+		width: 300px;
+		border: 1px solid white;
+		margin: 1rem 0;
+	}
+
+	button {
+		background-color: white;
+		color: #152a55;
+		padding: 0.5rem 1rem;
+		border-radius: 0.5rem;
+		cursor: pointer;
+	}
+</style>
